65667dee848184fecb312472c1139ccfc36bf550
Sunmi Seol <smseol@stanford.edu>
2021-02-19 23:52:58 -0800

    JS Home contact

--- main.js
+++ main.js
@@ -21,7 +21,21 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null) {
         return;
     } 
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView();
+    scrollIntoView(link);
 });
+
+// Handle scrolling when tapping "contact me" button
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    } 
+    scrollIntoView(link);
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+}
