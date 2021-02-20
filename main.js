acc2285189668c9d8e72f37915f02b10e9e23d42
Sunmi Seol <smseol@stanford.edu>
2021-02-20 01:39:25 -0800

    JS + Final

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ document.addEventListener('scroll', () => {
     }
 });
 
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -21,7 +22,14 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null) {
         return;
     } 
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click',() => {
+    navbarMenu.classList.toggle('open');
 });
 
 // Handle scrolling when tapping "contact me" button
